217310293d20280bfcd8fe7e3de36a061b705ab6
XDemonBloodX <tanguy.palmie@gmail.com>
2019-09-17 11:30:43 +0200

    ads++

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,14 +12,26 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
-
+       // $repo = $this->getDoctrine()->getRepository(Ad::class);
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
-            'ads' => $ads
-        ]);
+            'ads' => $ads]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     *
+     * @Route("ads/{slug}",name="ads_show")
+     * @return Reponse
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        //je recupere l'annonce par le slug
+        $ad=$repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig',['ad'=>$ad]);
     }
 }
