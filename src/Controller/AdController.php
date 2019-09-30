0453936262c086819f75504a61b6eaadd27e062a
XDemonBloodX <tanguy.palmie@gmail.com>
2019-09-30 15:30:04 +0200

    form lvl2

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -36,20 +36,15 @@ class AdController extends AbstractController
      */
     public function create(Request $request, ObjectManager $manager){
         $ad=new Ad();
-        $image= new Image();
-        $image->setUrl('http://placehold.it/400x200')
-              ->setCaption('Titre1');
-        
-        $image2= new Image();
-        $image2->setUrl('http://placehold.it/400x200')
-        ->setCaption('Titre2');
-
-        $ad->addImage($image)
-            ->addImage($image2);
 
         $form = $this->CreateForm(AdType::class, $ad);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
             $manager->persist($ad);
             $manager->flush();
             $this->addFlash(
