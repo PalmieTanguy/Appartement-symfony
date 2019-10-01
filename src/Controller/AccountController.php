ea131ee655e85f0130e969ab527b96099a305e75
XDemonBloodX <tanguy.palmie@gmail.com>
2019-10-01 12:15:20 +0200

    connexion

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/login", name="account_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error=$utils->getLastAuthenticationError();
+        return $this->render('account/login.html.twig',[
+            'hasError'=>$error !==null
+        ]);
     }
 }
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -29,7 +29,7 @@ class ImageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Image::class,
+            'data_class' => Image::class
         ]);
     }
 }
